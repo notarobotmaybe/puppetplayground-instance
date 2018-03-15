605f26f446001a73e341993d134237286b4a5213
Jordi Prats <jordi.prats@gmail.com>
2018-03-15 13:59:45 +0100

    verbose elasticsearch + single-node

--- elasticsearch/start_es.sh
+++ elasticsearch/start_es.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+ulimit -n 65536 
+ulimit -u 2048
+
+
 export ES_HOME=/usr/share/elasticsearch
 export ES_PATH_CONF=/etc/elasticsearch
 export PID_DIR=/var/run/elasticsearch
@@ -9,4 +13,4 @@ export ES_STARTUP_SLEEP_TIME=5
 
 cd /usr/share/elasticsearch
 
-su elasticsearch -c "/usr/share/elasticsearch/bin/elasticsearch -p ${PID_DIR}/elasticsearch.pid --quiet"
+su elasticsearch -c "/usr/share/elasticsearch/bin/elasticsearch -p ${PID_DIR}/elasticsearch.pid"
