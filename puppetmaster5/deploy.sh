9470a1ba1f83c4549e761147884dcbcc7a86bd41
Jordi Prats <jordi.prats@gmail.com>
2018-03-16 15:26:25 +0100

    dependencies r10k

--- puppetmaster5/deploy.sh
+++ puppetmaster5/deploy.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+PUPPETBIN=$(which puppet 2>/dev/null)
+
+if [ -z "$PUPPETBIN" ];
+then
+  if [ -e "/opt/puppetlabs/bin/puppet" ];
+  then
+    PUPPETBIN='/opt/puppetlabs/bin/puppet'
+  else
+    echo "puppet not found"
+    exit 1
+  fi
+fi
+
+
 R10KBIN=$(which r10k 2>/dev/null)
 if [ -z "$R10KBIN" ];
 then
@@ -12,5 +26,19 @@ then
   fi
 fi
 
-
+echo "Installing modules"
 $R10KBIN deploy environment $1 -p
+
+for PUPPET_ENVIRONMENT in $(find /etc/puppetlabs/code/environments/ -maxdepth 1 -mindepth 1 -type d | rev | cut -f1 -d/ | rev);
+do
+  echo -n "Installing dependencies for ${PUPPET_ENVIRONMENT}..."
+  # instala dependencies
+  for i in $($PUPPETBIN module list --environment ${PUPPET_ENVIRONMENT}  2>&1 | grep "Warning: Missing dependency" | cut -f2 -d\');
+  do
+    $PUPPETBIN module install $i --environment ${PUPPET_ENVIRONMENT} > /dev/null 2>&1
+  done
+  echo " OK"
+  $PUPPETBIN module list --environment ${PUPPET_ENVIRONMENT}
+  echo -e "\n"
+
+done
