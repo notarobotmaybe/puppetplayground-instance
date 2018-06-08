a5ff3ecae87cef350957bc2a01665f6b6983ea34
Jordi Prats <jordi.prats@gmail.com>
2018-06-08 12:45:31 +0200

    puppetdb

--- elasticsearch/extra/download.es.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ELASTIC_DEB=$(curl https://www.elastic.co/downloads/elasticsearch 2>/dev/null  | grep "zip-link" | grep ">deb<" | sed 's/.*href *= *"\?\([^" ]\+\)"\?.*/\1/')
-wget "${ELASTIC_DEB}" -O deb/es.deb
--- logstash/extra/download.logstash.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-LOGSTASH_DEB=$(curl https://www.elastic.co/downloads/logstash 2>/dev/null  | grep "zip-link" | grep -i ">deb" | sed 's/.*href *= *"\?\([^" ]\+\)"\?.*/\1/')
-wget "${LOGSTASH_DEB}" -O deb/logstash.deb
