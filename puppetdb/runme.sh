8184c3f3c686a40911b9ea154c8ffd1a85a118ec
Jordi Prats <jordi.prats@gmail.com>
2018-06-11 12:56:21 +0200

    postgres startup

--- puppetdb/runme.sh
+++ puppetdb/runme.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 PGDATA=/var/lib/pgsql/9.6/data
+
 # postgresql::hba_rule { 'IPv4/any puppetdb':
 #   user     => 'puppetdb',
 #   database => 'puppetdb',
@@ -14,4 +16,24 @@ PGDATA=/var/lib/pgsql/9.6/data
 #   owner => 'puppetdb',
 # }
 
-/usr/pgsql-9.6/bin/postmaster -D ${PGDATA}
+su - postgres -c "/usr/pgsql-9.6/bin/postmaster -D ${PGDATA}" &
+
+for i in $(seq 1 60);
+do
+  sleep 1
+
+  /usr/pgsql-9.6/bin/pg_isready > /dev/null 2>&1
+
+  IS_READY=$?
+
+  if [ $IS_READY -ne 0 ] && [ $i -eq 60 ];
+  then
+    echo "error starting postgres"
+    exit 1
+  fi
+
+  if [ $IS_READY -eq 0 ];
+  then
+    break;
+  fi
+done
